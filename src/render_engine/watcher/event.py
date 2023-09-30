abc44bbd19065b75c38a4a3935129ecbb5e53e9d
Mario Munoz <pythonbynight@gmail.com>
2023-09-30 09:50:37 -0700

    try closing and restarting server

--- src/render_engine/watcher/event.py
+++ src/render_engine/watcher/event.py
@@ -1,11 +1,10 @@
 import logging
-import time
-from typing import Any
-from watchdog.events import FileSystemEventHandler, FileSystemEvent
-from watchdog.observers import Observer
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 import socketserver
+import time
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
+from watchdog.observers import Observer
 
 logger = logging.getLogger(__name__)
 
@@ -38,13 +37,13 @@ class RenderEngineHandler(FileSystemEventHandler):
         logger.info(f'File {event.src_path} has been modified.')
         print(f'File {event.src_path} has been modified.')
 
-        # self.server.shutdown()
-        # time.sleep(1)
-        # logger.info(f'Server has been shutdown.')
-        # self.server.server_close()
-        # logger.info(f'Server has been closed. Attempting to reload...')
-        # httpd = socketserver.TCPServer(("localhost", 8000), self.request_handler)
-        # self.start_server()
+        self.server.shutdown()
+        time.sleep(1)
+        logger.info('Server has been shutdown.')
+        self.server.server_close()
+        logger.info('Server has been closed. Attempting to reload...')
+        socketserver.TCPServer(("localhost", 8000), self.request_handler)
+        self.start_server()
 
 
     def watch(self):
