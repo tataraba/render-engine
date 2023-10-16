bcbe075a79d3f709a00f291b2996f719ca42886a
Mario Munoz <pythonbynight@gmail.com>
2023-10-16 00:03:50 -0700

    add doc, remove prints

--- src/render_engine/watcher/event.py
+++ src/render_engine/watcher/event.py
@@ -1,8 +1,7 @@
 
 import threading
 import time
-from http.server import HTTPServer, SimpleHTTPRequestHandler, ThreadingHTTPServer
-from multiprocessing import Process
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 
 from rich.console import Console
 from watchdog.events import FileSystemEvent, RegexMatchingEventHandler
@@ -13,24 +12,15 @@ from render_engine import Site
 console = Console()
 
 
+def spawn_server(server_address: tuple[str, int], directory: str) -> ThreadingHTTPServer:
+    """
+	Create and return an instance of ThreadingHTTPServer that serves files
+    from the specified directory.
 
-
-# def server_func(server_address: tuple[int, int], directory: str):
-
-#     class _RequestHandler(SimpleHTTPRequestHandler):
-#         def __init__(self, *args, **kwargs):
-#             super().__init__(*args, directory=directory, **kwargs)
-
-#     def _httpd():
-#         console.print(f"Servin from server func: '{directory}' on http://{server_address[0]}:{server_address[1]}")
-#         httpd = HTTPServer(server_address, _RequestHandler)
-#         httpd.serve_forever()
-
-#     return _httpd
-
-
-def spawn_server(server_address: tuple[int, int], directory: str) -> ThreadingHTTPServer:
-
+    Args:
+    server_address: A tuple of a string and integer representing the server address (host, port).
+    directory: A string representing the directory from which the server should serve files.
+	"""
     class _RequestHandler(SimpleHTTPRequestHandler):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, directory=directory, **kwargs)
@@ -39,37 +29,45 @@ def spawn_server(server_address: tuple[int, int], directory: str) -> ThreadingHT
         return ThreadingHTTPServer(server_address, _RequestHandler)
     return _httpd()
 
+
 class RegExHandler(RegexMatchingEventHandler):
     """
-    Initializes a handler that matches given regexes with the
-    paths that are associated with the given events.
+    Initializes a handler that looks for file changes in a directory (`dir_to_watch`), as
+    well as creates a server to serve files in a given directory (`dir_to_serve`). The
+    class contains helper methods to manage server events in a thread.
 
-    Args:
-        render_engine_server (HTTPServer): Instance of render engine server.
-        server_address (tuple[int, int]): The server address.
-        app (Site): The Site instance of the application.
-        console (Console, optional): The Rich console for pretty printing of console messages. Defaults to Console().
-        patterns (list[str] | None, optional): Regexes to allow matching event paths. Defaults to None.
-        ignore_patterns (list[str] | None, optional): Regexes to ignore matching event paths. Defaults to None.
-        *args: Variable length argument list.
-        **kwargs: Arbitrary keyword arguments.
+    Meanwhile, the
+    `watch` method uses an instance of this handler class to monitor for file changes.
+    The files to be monitored are all files/directories within the `dir_to_watch`, which
+    defaults to the project root directory. The `patterns` and `ignore_patterns` are used
+    to filter the files to be monitored using regular expressions.
+
+    Params:
+        server_address: A tuple of the form (host, port)
+        dir_to_serve: The directory to serve
+        app: A Site instance
+        dir_to_watch: The directory to watch
+        patterns: A list of regular expressions to filter files
+        ignore_patterns: A list of regular expressions to ignore
     """
+
     def __init__(
         self,
         server_address: tuple[int, int],
         dir_to_serve: str,
         app: Site,
+        dir_to_watch: str = ".",
         patterns: [list[str] | None] = None,
         ignore_patterns: [list[str] | None] = None,
         *args,
         **kwargs
     ):
         self.p = None
-        # self.server_func = server_func(server_address, dir_to_serve)
-        self.server = spawn_server(server_address, dir_to_serve)
-        self.dir_to_serve = dir_to_serve,
+        self._server = spawn_server(server_address, dir_to_serve)
         self.server_address = server_address
+        self.dir_to_serve = dir_to_serve,
         self.app = app
+        self.dir_to_watch = dir_to_watch
         self.patterns = patterns
         self.ignore_patterns = ignore_patterns
         super().__init__(
@@ -81,42 +79,57 @@ class RegExHandler(RegexMatchingEventHandler):
 
 
     def start_server(self):
-        console.print("[bold green]Starting threaded server[/bold green]")
-        self._thread = threading.Thread(target=self.server.serve_forever)
+        console.print("[bold green]Starting server...[/bold green]")
+        self._server = spawn_server(self.server_address, self.dir_to_serve[0])
+        self._thread = threading.Thread(target=self._server.serve_forever)
         self._thread.start()
 
     def stop_server(self):
         console.print("[bold red]Stopping server[/bold red]")
-        self.server.shutdown()
-        self.server.server_close()
+        self._server.shutdown()
         self._thread.join()
 
     def rebuild(self):
-        console.print(f"[bold purple]Reloading and Rebuilding site...[/bold purple]")
+        console.print("[bold purple]Reloading and Rebuilding site...[/bold purple]")
         self.app.render()
 
 
     def on_any_event(self, event: FileSystemEvent):
-        print("HERERERERE")
         if event.is_directory:
             return None
         if event.event_type == "modified":
+
+            self.stop_server()
+            time.sleep(1)
             self.rebuild()
+            time.sleep(1)
+            self.start_server()
 
 
     def watch(self):
+        """
+        This function `watch` starts the server on the output path (`dir_to_serve`)
+        and monitors the specified directory (`dir_to_watch`) for changes.
+
+        After it starts the server, it "waits" and monitors the directory for
+        changes. If a change is detected, the `on_any_event` method is called,
+        which will stop the server and rebuild the site before restarting the
+        server.
+
+        If a KeyboardInterrupt is raised, it stops the observer and server.
+        """
 
         console.print(f'[yellow]Serving {self.app.output_path}[/yellow]')
 
         observer = Observer()
-        observer.schedule(self, ".", recursive=True)
+        observer.schedule(self, self.dir_to_watch, recursive=True)
         self.start_server()
         observer.start()
 
         try:
             while True:
                 print("waiting")
-                time.sleep(1)
+                time.sleep(3)
         except KeyboardInterrupt:
             console.print("watcher terminated by keystroke")
             observer.stop()
